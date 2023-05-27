5206739fa2fdb700c236ac92a910f97f8723f1d9
LeoSallu <L.sallustio99@gmail.com>
2023-05-27 11:44:08 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#Esercizio n°1
+
+name='Leonardo'
+print(name)
+
+#Esercizio n°2
+
+eta=24
+print(eta)
+
+#Esercizio n°3
+
+pi=3,14159
+print(pi)
+
+#Esercizio n°4
+
+width=10
+width=+15
+print(width)
+
+#Esercizio n°5
+
+lastname='Sallustio'
+full_name=name+lastname
+print(full_name)
+
+#Esercizio n°6
+
+future_eta=eta+10
+print(future_eta)
+
+#Esercizio n°7
+
+name_test='Franco'
+lastname_test='Pippo'
+year_of_birth=1999
+print(name_test,lastname_test,year_of_birth)
+
+#---------#
+
+name_test='Pluto'
+lastname_test='Baudo'
+year_of_birth=2000
+print(name_test,lastname_test,year_of_birth)
+
+
+#Esercizio n°8
+
+birth_year=1999
+actual_eta=2023-birth_year
+print('La tua età attuale è:',actual_eta)
